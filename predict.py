91d96e6020b88bbf4f9a7464f459607325b9ce87
Git Kurs User <git-kurs-user@datamics.com>
2019-08-13 13:34:41 +0200

    feature-13: fix change logging message

--- predict.py
+++ predict.py
@@ -6,6 +6,6 @@ tagger: SequenceTagger = SequenceTagger.load("ner")
 sentence: Sentence = Sentence("George Washington went to Washington .")
 tagger.predict(sentence)
 
-print("Analysing %s" % sentence)
+print("Analysing the sentence %s" % sentence)
 print("\nThe following NER tags are found: \n")
 print(sentence.to_tagged_string())
